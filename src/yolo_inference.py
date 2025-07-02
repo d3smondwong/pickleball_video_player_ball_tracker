9107de49d82afbcae5330420cf518d8d6b7d2570
d3smondwong <d3smondwong@gmail.com>
2025-07-02 14:57:18 +0800

    feat: To check results and load model from a folder

--- src/yolo_inference.py
+++ src/yolo_inference.py
@@ -1,8 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11x')
+model = YOLO('artifacts/models/yolo12x.pt')
 
 result = model.predict('input_videos/frames_00304_jpg.rf.1bfef6c4799a2e6fbb850802256667b3.jpg', save=True)
+
+# To see the results. Understand the the structure of the result object.
+# print(result)
+# print("Boxes:")
+# for box in result[0].boxes:
+#     print(box)
+
 
 # python -m src.yolo_inference
 
