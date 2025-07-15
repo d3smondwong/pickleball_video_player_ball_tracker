c118231536861fdf5873e38b231319b3072c94bd
d3smondwong <d3smondwong@gmail.com>
2025-07-15 15:03:06 +0800

    feat: Inculde video tracking on a video

--- src/yolo_inference.py
+++ src/yolo_inference.py
@@ -2,7 +2,11 @@ from ultralytics import YOLO
 
 model = YOLO('artifacts/models/yolo12x.pt')
 
-result = model.predict('input_videos/frames_00304_jpg.rf.1bfef6c4799a2e6fbb850802256667b3.jpg', save=True)
+# To perform inference on a single image, use the `predict` method.
+# result = model.predict('input_videos/frames_00304_jpg.rf.1bfef6c4799a2e6fbb850802256667b3.jpg', save=True)
+
+# To perform tracking on a video or a sequence of images, use the `track` method.
+result = model.track('input_videos/input_video.mp4', save=True)
 
 # To see the results. Understand the the structure of the result object.
 # print(result)
