c92b4558258f373813739603b5edb767f7c94638
d3smondwong <d3smondwong@gmail.com>
2025-07-16 13:55:17 +0800

    feat: Added in person bounding boxes in output video

--- src/app.py
+++ src/app.py
@@ -1,8 +1,23 @@
 from src.utils.video_utils import read_video, save_video
+from src.trackers.player_tracker import PlayerTracker
 from pathlib import Path
 
 def main():
-    # Read video frames from the input video file and save them to the output video file
+    """
+    Main function to process a video file.
+    This function performs the following steps:
+    1. Validates the existence and format of the input video file.
+    2. Reads video frames from the specified input video file.
+    3. Ensures the output directory exists or creates it if necessary.
+    4. Saves the read video frames to the specified output video file.
+    Raises:
+        FileNotFoundError: If the input video file does not exist.
+        ValueError: If the input path is not a file or has an unsupported format.
+    """
+
+    ###
+    # Read the frames from the input video file
+    ###
 
     # Ensure the input and output paths are correct
     input_video_path = Path("input_videos/input_video.mp4")
@@ -19,6 +34,25 @@ def main():
     print(f"Reading video frames from: {input_video_path}")
     video_frames = read_video(str(input_video_path))
 
+    ###
+    # Track the player using yolo12x
+    ###
+
+    # Load model and initiate PlayerTracker to track players for different frames
+    model_path = Path('artifacts/models/yolo12x.pt')
+    player_tracker = PlayerTracker(model_path=str(model_path))
+    player_detections = player_tracker.detect_frames(video_frames)
+
+    ###
+    # Draw bounding boxes on the output video frames
+    ###
+
+    # Draw bounding boxes on the video frames using the player detections
+    output_video_frames = player_tracker.draw_bounding_boxes(video_frames, player_detections)
+
+    ###
+    # Save video frames to the output video file
+    ###
     output_video_path = Path("output_videos/output_video.avi")
     print(f"Saving video frames to: {output_video_path}")
 
@@ -27,7 +61,7 @@ def main():
         output_video_path.parent.mkdir(parents=True, exist_ok=True)
 
     # Save the video frames to the output video file
-    save_video(video_frames, str(output_video_path))
+    save_video(output_video_frames, str(output_video_path))
 
 if __name__ == "__main__":
 
