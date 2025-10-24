b9c38ebafee7404d668088ad190cc53f35c310fb
d3smondwong <d3smondwong@gmail.com>
2025-10-24 15:06:16 +0800

    feat: Use conversion between feet and pixel

--- src/utils/conversions.py
+++ src/utils/conversions.py
@@ -1,5 +1,5 @@
-def convert_pixel_distance_to_meters(pixel_distance, refrence_height_in_meters, refrence_height_in_pixels):
-    return (pixel_distance * refrence_height_in_meters) / refrence_height_in_pixels
+def convert_pixel_distance_to_feet(pixel_distance, reference_height_in_feet, reference_height_in_pixels):
+    return (pixel_distance * reference_height_in_feet) / reference_height_in_pixels
 
-def convert_meters_to_pixel_distance(meters, refrence_height_in_meters, refrence_height_in_pixels):
-    return (meters * refrence_height_in_pixels) / refrence_height_in_meters
+def convert_feet_to_pixel_distance(feet, reference_height_in_feet, reference_height_in_pixels):
+    return (feet * reference_height_in_pixels) / reference_height_in_feet
